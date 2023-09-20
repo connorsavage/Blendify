2811df8416865f0248220fe47d27aeffa6d995b0
erinhurlburt <77364422+erinhurlburt@users.noreply.github.com>
2023-09-19 20:10:04 -0700

    access token info

--- blendify/src/App.js
+++ blendify/src/App.js
@@ -25,3 +25,15 @@ function App() {
 }
 
 export default App;
+
+
+// Access Token (Bearer):
+// BQAymk9Vq7utK6SICB9AYWxd1cULLmfLprbSytE1vb4a8wo_uRPhZe3DJ6aIqffnX7hF0AsH-HXYiOJtT3Sx_ia1XcTbvSAvR7csv0Ef_7-qyPXeTKM
+
+// curl command to generate new acces token:
+// curl -X POST "https://accounts.spotify.com/api/token" \
+// -H "Content-Type: application/x-www-form-urlencoded" \
+// -d "grant_type=client_credentials&client_id=7853b4c9dc604b2ea9b7f1cc305d1e86&client_secret=cad3689f3ed5446596b7105deed6497f"
+
+// client id: 7853b4c9dc604b2ea9b7f1cc305d1e86
+// client secret: cad3689f3ed5446596b7105deed6497f
