aa6546b7ac768739909eb99dcb13421e882b7f79
Connor Savage <csavage5@lion.lmu.edu>
2023-10-03 15:45:48 -0700

    fixed page views
    changed global overlapping variables, now we have separation between signin and home

--- blendify/src/Views/HomeView.js
+++ blendify/src/Views/HomeView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './HomeView.css';
 import SearchBar from './SearchBar'; // Make sure the path is correct
 
 export function HomeView({ onLogout }) {
@@ -9,14 +10,16 @@ export function HomeView({ onLogout }) {
     };
 
     return (
-        <div>
-            <h2>Home View!</h2>
-            <header>
-                <nav>
-                    <button onClick={onLogout}>Log Out</button>
-                </nav>
-            </header>
-            <SearchBar onSearch={handleSearch} />
+        <div className="home-view-wrapper">
+            <div>
+                <h2>Home View!</h2>
+                <header>
+                    <nav>
+                        <button onClick={onLogout}>Log Out</button>
+                    </nav>
+                </header>
+                <SearchBar onSearch={handleSearch} />
+            </div>
         </div>
     );
 }
