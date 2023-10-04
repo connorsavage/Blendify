f1a448c863f296af8cd64a6193d636044be2e491
Mitchell Cootauco <Mcootauc@lion.lmu.edu>
2023-10-03 22:43:55 -0700

    add _getToken and added token to searchbar & deleted history

--- blendify/src/Components/SearchBar/SearchBar.js
+++ blendify/src/Components/SearchBar/SearchBar.js
@@ -1,52 +1,67 @@
-import React, { useState } from 'react';
-import { createBrowserHistory } from 'history';
-
-import './SearchBar.css';
+import React, { useState } from "react"
+import "./SearchBar.css"
 
 const SearchBar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const history = createBrowserHistory();
+  const [searchTerm, setSearchTerm] = useState("")
+  const [searchResults, setSearchResults] = useState([])
 
-  const baseUrl = "https://api.spotify.com/v1";
-  //const token = "BQCC1dphvrPli2wJ1wDQV8uOT3Iuf5ExzUoPRt7xIiQzFsrBnZU1_8u6MIKK8_fYcLCzYSmJIyMh3xL3tdcHuE7zxa7cTQ3Z8ewx1smeNBPbM86RkoA";
-  const token = "BQBRDZw3ez4njbowbl-aGObMeQFAIQ5r800YrqRwEbMPS77LCagFIgqy4wHxZwtuuWlpjzjalzdYjnMtQIln0YrdoXDNJuSd_OaOQTYJr7wsupzr-KiFsjqvxWqA7M98ySrP-us5en9Pt2D6uG3F37q75UYBfTbtxY79Gl-11RoJeD1c6xSowrJ6VfCoWs0kUg";
+  const baseUrl = "https://api.spotify.com/v1"
+  //const token = "BQCC1dphvrPli2wJ1wDQV8uOT3Iuf5ExzUoPRt7xIiQzFsrBnZU1_8u6MIKK8_fYcLCzYSmJIyMh3xL3tdcHuE7zxa7cTQ3Z8ewx1smeNBPbM86RkoA"
+  // const token = "BQBRDZw3ez4njbowbl-aGObMeQFAIQ5r800YrqRwEbMPS77LCagFIgqy4wHxZwtuuWlpjzjalzdYjnMtQIln0YrdoXDNJuSd_OaOQTYJr7wsupzr-KiFsjqvxWqA7M98ySrP-us5en9Pt2D6uG3F37q75UYBfTbtxY79Gl-11RoJeD1c6xSowrJ6VfCoWs0kUg"
+  const clientId = "7853b4c9dc604b2ea9b7f1cc305d1e86"
+  const clientSecret = "cad3689f3ed5446596b7105deed6497f"
+
+  const _getToken = async () => {
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+      },
+      body: "grant_type=client_credentials",
+    })
+
+    const data = await result.json()
+    console.log("NEW TOKEN THAT WORKS!!!: " + data.access_token)
+    return data.access_token
+  }
 
   // search - q and type required
-    // q filters: artist, track, year, upc, tag:hipster, tag:new, isrc, genre
-      // tag:new - albums released in past two weeks
-      // tag:hipster - albums with lowest 10% popularity
-    // type filters: album, artist, playlist, track, show, episode, audiobook
+  // q filters: artist, track, year, upc, tag:hipster, tag:new, isrc, genre
+  // tag:new - albums released in past two weeks
+  // tag:hipster - albums with lowest 10% popularity
+  // type filters: album, artist, playlist, track, show, episode, audiobook
 
   const handleChange = (event) => {
-    setSearchTerm(event.target.value);    
-  };
+    setSearchTerm(event.target.value)
+  }
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    const token = await _getToken()
+
+    event.preventDefault()
 
     try {
-      const response = await fetch(`${baseUrl}/search?q=${searchTerm}&type=track`, { // ,album,artist,playlist
-        method: 'GET',
+      const response = await fetch(`${baseUrl}/search?q=${searchTerm}&type=track`, {
+        // ,album,artist,playlist
+        method: "GET",
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      });
+          Authorization: `Bearer ${token}`,
+        },
+      })
 
       if (!response.ok) {
-        throw new Error('Could not retrieve data');
+        throw new Error("Could not retrieve data")
       }
 
-      const data = await response.json();
+      const data = await response.json()
       // onSearch(data);
+      console.log("HERE'S THE DATA!!!: " + data.tracks.items[0].album.name)
       setSearchResults(data.tracks.items.name)
-
-      history.push(`/search/${encodeURIComponent(searchTerm)}&type=track}`);
-
     } catch (error) {
-      console.error('Error during API request: ', error);
+      console.error("Error during API request: ", error)
     }
-  };
+  }
 
   // const handleSubmit = (event) => {
   //   event.preventDefault();
@@ -55,10 +70,10 @@ const SearchBar = ({ onSearch }) => {
 
   return (
     <form className="search-bar" onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        placeholder="Search..." 
-        value={searchTerm} 
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
         onChange={handleChange}
       />
       <button type="submit">Search</button>
@@ -73,7 +88,7 @@ const SearchBar = ({ onSearch }) => {
         </div>
       )} */}
     </form>
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
