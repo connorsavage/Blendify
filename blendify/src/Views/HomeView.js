4954d51144566f8a16e31c9b995d8827924df230
agarren20 <argarren5@gmail.com>
2023-10-04 12:30:00 -0700

    LogOut functionality

--- blendify/src/Views/HomeView.js
+++ blendify/src/Views/HomeView.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import './HomeView.css';
-import SearchBar from './SearchBar'; // Make sure the path is correct
+import React, { useEffect } from "react"
+import "./HomeView.css"
+import { useNavigate } from "react-router-dom"
+import SearchBar from "./SearchBar" // Make sure the path is correct
 
-export function HomeView({ onLogout }) {
-    
-    const handleSearch = (searchTerm) => {
-        console.log("Searching for:", searchTerm);
-        // Implement your search logic here
-    };
+export function HomeView({ onLogout, processSignIn }) {
+  const navigate = useNavigate()
+  const handleSearch = (searchTerm) => {
+    console.log("Searching for:", searchTerm)
+    // Implement your search logic here
+  }
 
-    return (
-        <div className="home-view-wrapper">
-            <div>
-                <h2>Home View!</h2>
-                <header>
-                    <nav>
-                        <button onClick={onLogout}>Log Out</button>
-                    </nav>
-                </header>
-                <SearchBar onSearch={handleSearch} />
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    processSignIn()
+    //new URLSearchParams(window.location.search)
+  }, [])
+
+  return (
+    <div className="home-view-wrapper">
+      <div>
+        <h2>Home View!</h2>
+        <header>
+          <nav>
+            <button
+              onClick={() => {
+                onLogout()
+                navigate("/signin")
+              }}
+            >
+              Log Out
+            </button>
+          </nav>
+        </header>
+        <SearchBar onSearch={handleSearch} />
+      </div>
+    </div>
+  )
 }
