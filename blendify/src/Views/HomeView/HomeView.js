f67bd7e27740d9709f1366d794554137e87f084d
Sebastian Cruz <scruz17@lion.lmu.edu>
2023-10-15 21:57:44 -0700

    Creating Sidebar
    creating sidebar, some css elements adjusted (for now), pages still need to be made

--- blendify/src/Views/HomeView/HomeView.js
+++ blendify/src/Views/HomeView/HomeView.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import "./HomeView.css"
 import { useNavigate } from "react-router-dom"
-import SearchBar from "../../Components/SearchBar/SearchBar" // Make sure the path is correct
+import SearchBar from "../../SearchBar/SearchBar" // Make sure the path is correct
+import Navbar from "../../Components/Navbar"
+import {GiHamburgerMenu} from 'react-icons/gi'
 
 export function HomeView({ onLogout, processSignIn }) {
+  const [ showNav, setShowNav ] = useState(false)
+
+
   const navigate = useNavigate()
   const handleSearch = (searchTerm) => {
     console.log("Searching for:", searchTerm)
@@ -17,19 +22,21 @@ export function HomeView({ onLogout, processSignIn }) {
 
   return (
     <div className="home-view-wrapper">
+      <header>
+        <GiHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+        <nav>
+          <button
+            onClick={() => {
+              onLogout()
+              navigate("/signin")
+            }}
+          >
+            Log Out
+          </button>
+        </nav>
+      </header>
+      <Navbar show={showNav} />
       <div className="home-view">
-        <header>
-          <nav>
-            <button
-              onClick={() => {
-                onLogout()
-                navigate("/signin")
-              }}
-            >
-              Log Out
-            </button>
-          </nav>
-        </header>
         <div className="search-text">Find Your Blend</div>
         <SearchBar onSearch={handleSearch} />
       </div>
