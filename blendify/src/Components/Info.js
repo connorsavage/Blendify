55630f6779a82e03ab7a0b880a5246f593ce7fda
ohunger <owenhunger@hotmail.com>
2023-11-09 20:14:29 -0800

    lots of css edits for new page, new google, youtube, spotify buttons

--- blendify/src/Components/Info.js
+++ blendify/src/Components/Info.js
@@ -8,6 +8,53 @@ export default function Info({ searchId, musicData }) {
   const clientId = "7853b4c9dc604b2ea9b7f1cc305d1e86"
   const clientSecret = "cad3689f3ed5446596b7105deed6497f"
 
+  function searchGoogle(query) {
+    // Construct the Google search URL with the query
+    const searchURL = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+    
+    // Open a new tab with the Google search URL
+    window.open(searchURL, '_blank');
+  }
+  
+  // Attach a click event listener to the list items
+  document.addEventListener('click', function (event) {
+    const listItem = event.target.closest('.recommendations-list');
+    
+    if (listItem) {
+      const text = listItem.textContent.trim(); // Get the text from the list item
+      searchGoogle(text); // Perform the Google search
+    }
+  });
+
+
+  function searchYouTube(query) {
+    // Construct the YouTube search URL with the query
+    const searchURL = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
+    
+    // Open a new tab with the YouTube search URL
+    window.open(searchURL, '_blank');
+  }
+  
+  // Rest of your code ...
+  
+  // Attach a click event listener to the list items
+  document.addEventListener('click', function (event) {
+    const listItem = event.target.closest('.recommendations-list');
+    
+    if (listItem) {
+      const text = listItem.textContent.trim(); // Get the text from the list item
+      searchYouTube(text); // Perform the YouTube search
+    }
+  });
+
+  function searchSpotify(query) {
+    // Construct the Spotify search URL with the query
+    const searchURL = `https://open.spotify.com/search/${encodeURIComponent(query)}`;
+    
+    // Open a new tab with the Spotify search URL
+    window.open(searchURL, '_blank');
+  }
+
   const handleSongClick = async (song) => {
     setSelectedSong(song);
     console.log("song", song);
@@ -85,18 +132,31 @@ export default function Info({ searchId, musicData }) {
         )
       ) : (
         <div>
-          <button onClick={goBack}>Back</button>
-          <p>
-            <span className="song-name">{selectedSong.name}</span> by{" "}
-            <span className="artist-name">{selectedSong.artists.map((artist) => artist.name).join(", ")}</span>
-          </p>  
+          <button className="back-button" onClick={goBack}>Back</button>
+          <div>
+            <p classname="selected-song">
+              <span className="song-name">{selectedSong.name}</span> by{" "}
+              <span className="artist-name">{selectedSong.artists.map((artist) => artist.name).join(", ")}</span>
+            </p> 
+          </div>
+           
           {console.log("song recs", songRecs)}
           {songRecs && (
             <div>
-              <h3>Song Recommendations</h3>
-              <ul>
+              <h3 className="song-recommendations">Song Recommendations for BPM: </h3>
+              <ul >
                 {songRecs.tracks.map((rec) => (
-                  <li key={rec.id}>{rec.name}</li>
+                  <li className="recommendations-list" key={rec.id} >
+                     <div className="text-container">
+                        <span className="song-name">{rec.name}</span> by{" "}
+                        <span className="artist-name">*artist*</span>
+                      </div>
+                      <div className="button-container">
+                        <button className="google-button" onClick={() => searchGoogle(`${rec.name} by ${rec.artist}`)}>Google</button>
+                        <button className="youtube-button" onClick={() => searchYouTube(`${rec.name} by ${rec.artist}`)}>YouTube</button>
+                        <button className="spotify-button" onClick={() => searchSpotify(`${rec.name} by ${rec.artist}`)}>Spotify</button>
+                      </div>
+                  </li>
                 ))}
               </ul>
             </div>
